1d29e1cd2bd87edb16152c2e4e19b2034c39d6e4
zenraiching <rickdavidsangalang@gmail.com>
2021-05-13 00:32:43 +0800

    created notebook and calculate

--- generate.py
+++ generate.py
@@ -1,11 +1,17 @@
 import csv
 import random
-
-with open('innovators.csv', 'w', newline='') as file:
+listOfWorks = ["Quality Assurance", "Software developer",
+               "Quality Engineer", "Test engineer", "UI/UX design",
+               "Java Developer", "Python Developer",
+               "Frontend Developer", "Database Manager"
+               ]
+with open('dataset.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-
+    writer.writerow(["Gender", "Age", "Work", "College"])
     for i in range(100):
         gender = random.randint(0, 1)
         age = random.randint(18, 25)
-        writer.writerow([gender, age])
+        work = random.choice(listOfWorks)
+        isCollege = random.randint(0, 1)
+        writer.writerow([gender, age, work, isCollege])
     # end of the file
